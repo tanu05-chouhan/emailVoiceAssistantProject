c8205d49fe3dd8d38a39b6034597b5103debc403
Tanu Chouhan <70907712+tanu05-chouhan@users.noreply.github.com>
2022-01-04 09:20:28 -0500

    Add files via upload

---
+++ emailVoiceAssistant.py
@@ -0,0 +1,28 @@
+# Tanu Chouhan
+# Email Voice Assistant AI Project
+
+import speech_recognition as sr
+import yagmail
+
+recognizer=sr.Recognizer()
+with sr.Microphone() as source:
+    print('Clearing background noise..')
+    recognizer.adjust_for_ambient_noise(source,duration=1)
+    print("waiting for your message...")
+    recordedaudio=recognizer.listen(source)
+    print('Done recording..!')
+try:
+    print('Printing the message..')
+    text=recognizer.recognize_google(recordedaudio,language='en-in')
+
+    print('Your message:{}'.format(text))
+
+except Exception as ex:
+    print(ex)
+
+#Automate mails:
+
+reciever='kashyapsumedha19@gmail.com'
+message=text
+sender=yagmail.SMTP('tanu05chouhan@gmail.com')
+sender.send(to=reciever,subject='This is an automated mail',contents=message)
